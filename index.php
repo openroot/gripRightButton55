0e8599453352504e3c22858c0f899744993548e3
Debaprasad Tapader <dev.openroot@gmail.com>
2024-07-19 01:43:54 +0530

    update

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
 		<title>Bootstrap Example</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="plugin/bootstrap-4.0.0-dist/css/bootstrap.min.css">
-		<script src="plugin/jquery-3.7.1-dist/jquery-3.7.1.min.js"></script>
-		<script src="plugin/popper-1.12.8-dist/popper-1.12.8.min.js"></script>
-		<script src="plugin/bootstrap-4.0.0-dist/js/bootstrap.bundle.min.js"></script>
+		<link rel="stylesheet" href="interNet29/plugin/bootstrap-4.0.0-dist/css/bootstrap.min.css">
+		<script src="interNet29/plugin/jquery-3.7.1-dist/jquery-3.7.1.min.js"></script>
+		<script src="interNet29/plugin/popper-1.12.8-dist/popper-1.12.8.min.js"></script>
+		<script src="interNet29/plugin/bootstrap-4.0.0-dist/js/bootstrap.bundle.min.js"></script>
 	</head>
 	<body>
 		<div class="jumbotron text-center">
 			<h1>My First Bootstrap Page</h1>
-			<p>Resize this responsive page to see the effect!</p> 
-		</div>  
+			<p>Resize this responsive page to see the effect!</p>
+		</div>
 		<div class="container">
 			<div class="row">
 			<div class="col-sm-4">
