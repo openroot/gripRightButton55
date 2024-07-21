00a6220a7bed98fd00a2bbbf1d0e63ae110ef025
Debaprasad Tapader <dev.openroot@gmail.com>
2024-07-21 09:11:50 +0530

    update

--- index.php
+++ index.php
@@ -14,29 +14,59 @@
 		</div>
 
 		<?php
+			set_error_handler(function($errNo, $errStr, $errFile, $errLine) {
+				if (0 === error_reporting()) {
+					return false;
+				}
+				throw new ErrorException($errStr, 0, $errNo, $errFile, $errLine);
+			});
 
-			
+			function displayError($error) {
+				print "<br>-<br>" . $error . "<br>-<br>";
+			}
 
+			class readFile {
+				public $name;
+				public $content;
+				public $size;
+
+				function __construct($name) {
+					$this->name = $name;
+					$this->read();
+				}
+
+				private function read() {
+					$file = null;
+					try {
+						$file = fopen($this->name, "r");
+					}
+					catch (Exception $e) {
+						displayError($e);
+					}
+					if ($file) {
+						$this->size = filesize($this->name);
+						if ($this->size > 0) {
+							$this->content = fread($file, $this->size);
+							fclose($file);
+						}
+					}
+				}
+
+				public function displayData() {
+					print "File name: " . $this->name . "<br>";
+					print "File size: " . $this->size . "<br>";					
+					$this->content = mb_convert_encoding($this->content, "UTF-8", "HTML-ENTITIES");
+					$this->content = nl2br(htmlspecialchars($this->content));
+					$this->content = str_replace(" ", "&nbsp;", $this->content);
+					print "File content:<br>" . $this->content;
+					print "<br>";
+				}
+
+				function __destruct() { }
+			}
+
+			$file = new readFile("o");
+			$file->displayData();
 		?>
-
-		<div>
-			<div>
-				<div>
-					<h3>Column 1</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-				</div>
-				<div>
-					<h3>Column 2</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-				</div>
-				<div>
-					<h3>Column 3</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-				</div>
-			</div>
-		</div>
 	</body>
 </html>
