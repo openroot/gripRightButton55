893e90d87bef58033a2be320b6082d5d75a102ef
Debaprasad Tapader <dev.openroot@gmail.com>
2024-08-10 18:28:58 +0530

    update

--- comPile31/move15.php
+++ comPile31/move15.php
@@ -21,20 +21,17 @@
 		private function scanDirectories(string $path) {
 			if (is_dir($path)) {
 				$directoryList = [];
-				$fileList = [];
 				foreach (scandir($path) as $value) {
 					if (!in_array($value, $this->extraReservedName, true)) {
 						if (is_dir($path . "\\" . "$value") && !in_array($value, $this->extraDirectoryName, true)) {
-							array_push($directoryList, $path . "\\" . $value);
 							$this->hologramDirectoryPath[$path . "\\" . $value] = $value;
+							array_push($directoryList, $path . "\\" . $value);
 						}
 						if (is_file($path . "\\" . "$value") && !in_array($value, $this->extraFileName, true)) {
-							array_push($fileList, $path . "\\" . $value);
 							$this->hologramFilePath[$path . "\\" . $value] = $value;
 						}
 					}
 				}
-
 				if (count($directoryList) > 0) {
 					foreach ($directoryList as $directoryPath) {
 						$this->scanDirectories($directoryPath);
