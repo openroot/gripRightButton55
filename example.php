e7a5e490fa2b25f82e0a5a0ba657df42bc8c5d5e
Debaprasad Tapader <dev.openroot@gmail.com>
2024-08-16 19:54:10 +0530

    update

---
+++ example.php
@@ -0,0 +1,113 @@
+<?php
+	require_once("comPile31/save13.php");
+	require_once("comPile31/buy13.php");
+	require_once("comPile31/move15.php");
+	require_once("comPile31/tor12.php");
+
+	use comPile31\save13 as save13;
+	use comPile31\buy13 as buy13;
+	use comPile31\move15 as move15;
+	use comPile31\tor12 as tor12;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>gripRightButton55</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="interNet29/plugIn128/n.umber/style.css">
+		<script src="interNet29/plugIn128/n.umber/script.js"></script>
+	</head>
+	<body>
+		<div id="spectrum"></div>
+
+		<div class="dustParticle">
+			<div>
+				<div class="starSecond"></div>
+				<div class="starThird"></div>
+				<div class="starFourth"></div>
+				<div class="starFifth"></div>
+			</div>
+		</div>
+
+		<div class="frame">
+		<?php
+			$fault = new tor12\fault();
+			try {
+				$example = new tor12\example();
+				$example->plainText();
+				$example->htmlSegment();
+				$example->htmlPre();
+				$example->htmlTable();
+				$example->error();
+			}
+			catch (Exception $e) {
+				$fault->show([$e]);
+			}
+		?>
+		</div>
+
+		<div id="menu">
+			<div>
+				<table>
+					<caption>
+						<div>
+							gripRightButton55
+						</div>
+						<div>
+							Leverage Any Complications
+						</div>
+					</caption>
+					<tr>
+						<td>A</td>
+						<td>B</td>
+						<td>C</td>
+						<td>D</td>
+						<td>E</td>
+					</tr>
+					<tr>
+						<td>F</td>
+						<td>G</td>
+						<td>H</td>
+						<td>I</td>
+						<td>J</td>
+					</tr>
+					<tr>
+						<td>K</td>
+						<td>L</td>
+						<td>M</td>
+						<td>N</td>
+						<td>O</td>
+					</tr>
+					<tr>
+						<td>P</td>
+						<td>Q</td>
+						<td>R</td>
+						<td>S</td>
+						<td>T</td>
+					</tr>
+					<tr>
+						<td>U</td>
+						<td>V</td>
+						<td>W</td>
+						<td>X</td>
+						<td>Y</td>
+					</tr>
+					<tr>
+						<td>Z</td>
+						<td>_</td>
+						<td>@</td>
+						<td>&lt;</td>
+						<td>&gt;</td>
+					</tr>
+				</table>
+			</div>
+			<div>
+				&copy; D Tapader 
+				<a href="https://github.com/openroot" target="_blank">g</a> 
+				<a href="https://www.facebook.com/larrypage34" target="_blank">f</a>
+			</div>
+		</div>
+	</body>
+</html>
